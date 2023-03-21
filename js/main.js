9dcd4db2396f3ccf99fa5ec98ab558003d4ceec5
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-21 15:29:28 +0100

    .

--- js/main.js
+++ js/main.js
@@ -7,7 +7,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-        hello: 'Ciao Mondo!'
+      apiRandomMail: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      mailNumber: 10,
+      mailList: []
     }
+  },
+  methods: {
+    // ESTRAI DATI DA UN API X NUMERO DI VOLTE E INSERISCILI IN UN ARRAY
+    repeatOperation(number, indirizzo, array) {
+      for (let i = 0; i < number; i++) {
+        axios.get(indirizzo)
+          .then((response) => {
+            array.push(response.data.response)
+          });
+      }
+    }
+  },
+  beforeMount() {
+    // PRIMA CHE IL PROGRAMMA VENGA MONTATO ESEGUI QUESTO METODO 
+    this.repeatOperation(this.mailNumber, this.apiRandomMail,this.mailList)
   }
 }).mount('#app');
