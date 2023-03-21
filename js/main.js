cf65346559bde9ab75e4c65320726d8a6c6ec20f
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-21 16:31:40 +0100

    Aggiunto loading, progetto finito

--- js/main.js
+++ js/main.js
@@ -7,24 +7,41 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      visible: false,
       apiRandomMail: 'https://flynn.boolean.careers/exercises/api/random/mail',
       mailNumber: 10,
       mailList: []
     }
   },
   methods: {
-    // ESTRAI DATI DA UN API X NUMERO DI VOLTE E INSERISCILI IN UN ARRAY
-    repeatOperation(number, indirizzo, array) {
-      for (let i = 0; i < number; i++) {
-        axios.get(indirizzo)
-          .then((response) => {
-            array.push(response.data.response)
-          });
+
+    // FACCIO UNA RICHESTA AL SERVER 
+    request(indirizzo, array) {
+      axios.get(indirizzo)
+        .then((response) => {
+          array.push(response.data.response)
+          this.showList()
+        })
+    },
+
+    // FUNZIONE CHE VISUALIZZA LA LISTA 
+    showList() {
+      if (this.mailList.length === this.mailNumber) {
+        this.visible = true;
       }
-    }
+    },
+    
+    // ESTRAI DATI DA UN API X NUMERO DI VOLTE E INSERISCILI IN UN ARRAY
+    repeatRequest(number) {
+      for (let i = 0; i < number; i++) {
+        this.request(this.apiRandomMail, this.mailList)
+      }
+    },
+
   },
   beforeMount() {
     // PRIMA CHE IL PROGRAMMA VENGA MONTATO ESEGUI QUESTO METODO 
-    this.repeatOperation(this.mailNumber, this.apiRandomMail,this.mailList)
+    this.repeatRequest(this.mailNumber)
+
   }
 }).mount('#app');
